492bdff0c141c33bef907660492e05e1c3ec8f18
MehdiDgrs <Mehdigital@outlook.com>
2022-06-29 12:10:48 +0200

    initial commit

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = {
+    //
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 15,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+  };
+  
+   
+  
+   
