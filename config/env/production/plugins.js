a61673a39791db920af91f5e58d324eff7f64e16
MehdiDgrs <Mehdigital@outlook.com>
2022-06-30 16:08:20 +0200

    modified for production

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = ({env})=> ({
     //
     graphql: {
       config: {
@@ -12,7 +12,21 @@ module.exports = {
         },
       },
     },
-  };
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
   
    
   
